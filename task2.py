a22a999b327cb59a9113579345e6529f8987c5e4
Veronika228 <mynik1606@gmail.com>
2020-04-10 20:07:21 +0300

    end les1

--- task2.py
+++ task2.py
@@ -1,9 +1,9 @@
 seconds = int(input("Введите время в секундах.\n"))
 
 seconds: int = seconds
-minutes: int = seconds / 60
-hours: int = (seconds / 60) / 60
+minutes: int = seconds / 3600
+hours: int = (seconds / 3600) / 60
 
-result = '{0}:{1}:{2}'.format(seconds, minutes, hours)
+result = '{0}:{1}:{2}'.format(hours, minutes, seconds)
 
 print(result)
